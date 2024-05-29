82d7f667b6b2af17d1ae2eb9c952ba3a9966849b
ETRI우예린 <yrwoo@etri.re.kr>
2024-05-29 18:21:40 +0900

    fix: ingesting grepp data

--- src/lib/grepp.py
+++ src/lib/grepp.py
@@ -1,55 +1,103 @@
-import subprocess
+import requests, json
+from tqdm import tqdm
 from lib.config import Account
+from lib.logging_ import to_file
 
 class Grepp:
-    def __init__(self) -> None:
-        self.base = 'https://etri.programmers.co.kr/api/v1/etri/'
+    def __init__(self):
+        self.base = 'https://etri.programmers.co.kr/api/v1/etri'
+        self.token = self.get_token()
     
-    def session(self):
-        cmd = f"""
-                    curl -X 'POST' '{self.base}/login' \
-                    -H 'accept: application/json' \
-                    -H 'Content-Type: application/json' \
-                    -d '{
-                        "email": {Account['EMAIL']},
-                        "password": {Account['PW']}
-                    }'
-                """
-        token = subprocess.run(
-            cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE
-                               ).stdout.decode("utf-8")
-        return token
-
-    def list_challenges(self, level, language, volume):
-        token = self.session()
-        print(token)
-
-        # cmd = f"""
-        #         curl -X 'GET' \
-        #         '{self.base}challenges?languages%5B%5D={language}&levels%5B%5D={level}&page=1&per_page={volume}' \
-        #         -H 'accept: application/json'\
-        #         -H 'Authorization: Bearer {token}'
-        #     """
-        # response = subprocess.run(
-        #     cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE
-        #                             ).stdout.decode("utf-8")
-        # print(response)
-        # problem_list = []
-        # for i in range(len(response.challenges)):
-        #     print()
-        
+    def get_token(self):
+        try:
+            response = requests.post(f'{self.base}/login', json={'email':Account['EMAIL'], 'password':Account["PW"]},)
+        except SystemError:
+            print(f'{response}')
+        return response.json()['token']
+    
+    def list_challenges(self, language):
+        PER_PAGE = 50
+        # PER_PAGE = 2
+        print(self.token)
+        response = requests.get(
+            f'{self.base}/challenges', 
+            headers= {
+                'Authorization': f'Bearer {self.token}',
+                'Content-type': 'application/json',
+            },
+            params={
+                'per_page': PER_PAGE
+            }
+        ).json()
+        challenges = []
+        for page in tqdm(range(response['totalPages'])):
+        # for page in range(2):
+            tmp = requests.get(
+                        f'{self.base}/challenges', 
+                        headers= {
+                            'Authorization': f'Bearer {self.token}',
+                            'Content-type': 'application/json',
+                        },
+                        params= {
+                            'page': page+1,
+                            'perPage': PER_PAGE,
+                        }
+                    ).json()
+            for i in tqdm(range(len(tmp['challenges']))):
+                print(tmp['challenges'][i]['id'])
+                detail = self.get_challenge(tmp['challenges'][i]['id'])
+                solutions = self.get_solution_groups(tmp["challenges"][i]['id'])
+                # print(solutions)
+                tmp['challenges'][i].update(detail)
+                tmp['challenges'][i].update(solutions)
+            to_file(f'../data/grepp-{page}.json', tmp, type='json')
     
     def get_challenge(self, id): # 
-        array = [
-          [], # 0 pNumber
-          [], # 1 title
-          [], # 2 level
-          [], # 3 description
-          [], # 4 example 
-          [], # 5 constraint
-          [], # 6 output
-        ]
-        print()
+        response = requests.get(
+            f'{self.base}/challenges/{id}', 
+            headers= {
+                'Authorization': f'Bearer {self.token}',
+                'Content-type': 'application/json',
+            },
+        ).json()
+        try:
+            del response['id']
+        except KeyError:
+            print('Key "id" does not exist in the response')
+        
+        return response
 
-    def get_solution_groups():
-        print()
+    def get_solution_groups(self, id):
+        solutions = {}
+        response = requests.get(
+            f'{self.base}/challenges/{id}/solution-groups', 
+            headers= {
+                'Authorization': f'Bearer {self.token}',
+                'Content-type': 'application/json',
+            },
+        ).json()
+        
+        try:
+            solutions["solutionGroups"] = response["solutionGroups"]
+        except KeyError:
+            print('Key "solutionGroups" does not exist in the response')
+        return solutions
+
+    def refine_data(self, path):
+        array = [
+            [], # 0 pNumber
+            [], # 1 title
+            [], # 2 partTitle
+            [], # 3 languages
+            [], # 4 level 
+            [], # 5 description
+            [], # 6 example
+            [], # 7 constraint
+            [], # 8 testcases
+            [], # 9 solution-python
+        ]
+        with open(f'{path}', 'r') as f:
+            json_data = json.load(f)
+        # print(json_data)
+        problems = json_data['challenges']
+        print(len(problems))
