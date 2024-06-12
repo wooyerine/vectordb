d918a14e99cbf1c257d407d806d410925146b973
ETRI우예린 <yrwoo@etri.re.kr>
2024-06-12 20:13:00 +0900

    fix: add query cross platform

--- src/qa.py
+++ src/qa.py
@@ -9,9 +9,10 @@ openai.api_key = Openai['API_KEY']
 
 def arg_parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--service', type=str, choices=['leetcode', 'grepp'])
-    parser.add_argument('--language', default='python3', type=str)
     parser.add_argument('--query', type=str, help='problem id of leetcode or problem title of grepp')
+    parser.add_argument('--service', type=str, choices=['leetcode', 'grepp'])
+    parser.add_argument('--collection', type=str, choices=['leetcode', 'grepp'])
+    parser.add_argument('--language', default='python3', type=str)
     args = parser.parse_args()
 
     return args
@@ -19,15 +20,16 @@ def arg_parse():
 if __name__ == '__main__':
     args = arg_parse()
     milvus = VectorDB()
-
-    collection = milvus.connect_collection(args.service)
+    search_collection = milvus.connect_collection(args.collection)
     if args.service == 'leetcode':
+        collection = milvus.connect_collection("leetcode")
         leetcode = Leetcode()
         problem = json.loads(leetcode.get_problem(args.query, args.language, './result/query/leetcode'))
-        result = milvus.search(collection=collection, data=problem["description"], target='desc_embedding')
+        result = milvus.search(collection=search_collection, data=problem["description"], target='desc_embedding')
         to_file('./result/query/leetcode', f'response-{args.query}.json', result, 'json')
     if args.service == 'grepp':
+        collection = milvus.connect_collection("grepp")
         grepp = Grepp()
         problem = milvus.query(collection=collection, expr=f'title == "{args.query}"')
-        result = milvus.search(collection=collection, data=problem[0]["description"], target='desc_embedding')
+        result = milvus.search(collection=search_collection, data=problem[0]["description"], target='desc_embedding')
         to_file('./result/query/grepp', f'response-{args.query}.json', result, 'json')
