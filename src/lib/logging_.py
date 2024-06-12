25943d60e34fa0bfb021d103f4753bbbfb836570
ETRI우예린 <yrwoo@etri.re.kr>
2024-06-12 15:12:08 +0900

    fix: trimming

--- src/lib/logging_.py
+++ src/lib/logging_.py
@@ -1,18 +1,12 @@
 import os, json, time
 
 def to_file(directory, file_name, data, data_type=None):
-  current_time = time.localtime()
-  formatted_time = time.strftime('%Y%m%d-%H%M%S', current_time)
-  path = f'{directory}'
-  # base, extension = os.path.splitext(f'{directory}/{file_name}')
   if not os.path.exists(directory):
       os.makedirs(directory)
-  while os.path.exists(f'{directory}'):
-      path = f'{directory}_{formatted_time}'
-      os.makedirs(path)
+  
   if data_type == 'json':
-    with open(f'{path}/{file_name}', "w", encoding='utf-8') as json_file:
+    with open(f'{directory}/{file_name}', "w", encoding='utf-8') as json_file:
       json.dump(data, json_file, ensure_ascii=False)
   else:
-    with open(f'{path}/{file_name}', 'w') as f:
+    with open(f'{directory}/{file_name}', 'w') as f:
       f.write(data)
